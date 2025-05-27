ea97301462feeef9a92adee84c2f9e4f41ff4b6f
Kadirov Kamoliddin <154986483+Kadirovs@users.noreply.github.com>
2025-05-28 00:06:09 +0400

    git commit

---
+++ src/Application/Contracts/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+namespace Application.Contracts;
+
+public interface IStudentService
+{
+    List<Student> GetStudents();
+    Student? GetStudentById(int id);
+    bool CreateStudent(Student student);
+    bool UpdateStudent(Student student);
+    bool DeleteStudent(int id);
+    
+    
+}
---
+++ src/Infrastructure/Implementations/StudentService.cs
@@ -0,0 +1,33 @@
+﻿using Application.Contracts;
+using Domain.Entities;
+using Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+namespace Infrastructure.Implementations;
+
+public class StudentService : IStudentService
+{
+    public List<Student> GetStudents()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Student? GetStudentById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool CreateStudent(Student student)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateStudent(Student student)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteStudent(int id)
+    {
+        throw new NotImplementedException();
+    }
+}

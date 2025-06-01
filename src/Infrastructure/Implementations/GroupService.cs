1614b31c5446e97f505c7c713d823f59b78eb465
Kadirov Kamoliddin <154986483+Kadirovs@users.noreply.github.com>
2025-06-02 00:48:17 +0400

    git commit

--- src/Infrastructure/Implementations/GroupService.cs
+++ src/Infrastructure/Implementations/GroupService.cs
@@ -1,8 +1,33 @@
 ﻿using Application.Contracts;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Implementations;
 
-public sealed class GroupService:IGroupService
+public sealed class GroupService(DbContext dbContext):IGroupService
 {
-    
+    public List<Group> GetGroups()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Group? GetGroupById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool CreateGroup(Group group)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool UpdateGroup(Group group)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteGroup(int id)
+    {
+        throw new NotImplementedException();
+    }
 }

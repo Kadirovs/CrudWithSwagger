84446fc36667cdffe0d925076f13b7b3d0623a11
Kadirov Kamoliddin <154986483+Kadirovs@users.noreply.github.com>
2025-06-03 00:16:53 +0400

    git commit

---
+++ src/WebApp/Controllers/GroupController.cs
@@ -0,0 +1,54 @@
+﻿using Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Domain.Entities;
+
+namespace WebApp.Controllers;
+[Route("api/groups")]
+[ApiController]
+
+public sealed class GroupController(IGroupService groupService):ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(groupService.GetGroups());
+    }
+
+    [HttpGet("{id:int}")]
+
+    public IActionResult Get(int id)
+    {
+        Group? group = groupService.GetGroupById(id);
+        if (group is null)
+            return NotFound();
+        return Ok(group);
+        
+    }
+    
+    [HttpPost]
+    public IActionResult Create([FromBody] Group group)
+    {
+        bool res = groupService.CreateGroup(group);
+        if (res == false)
+            return BadRequest("Group already exist");
+        return Ok(res);
+    }
+    
+    [HttpPut]
+    public IActionResult Update([FromBody] Group group)
+    {
+        bool res = groupService.UpdateGroup(group);
+        if (res == false)
+            return NotFound("Group not found.");
+        return Ok(res);
+    }
+    
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        bool res = groupService.DeleteGroup(id);
+        if (res == false)
+            return BadRequest("Group not found.");
+        return Ok(res);
+    }
+}

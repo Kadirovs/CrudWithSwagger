a4cee1e2c528f8401de741958b3b9a7b3e1f54ec
Kadirov Kamoliddin <154986483+Kadirovs@users.noreply.github.com>
2025-06-02 23:47:44 +0400

    git commit

--- src/Infrastructure/Implementations/GroupService.cs
+++ src/Infrastructure/Implementations/GroupService.cs
@@ -1,33 +1,57 @@
 ﻿using Application.Contracts;
 using Domain.Entities;
+using Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Implementations;
 
-public sealed class GroupService(DbContext dbContext):IGroupService
+public sealed class GroupService(DataContext dbContext) : IGroupService
 {
     public List<Group> GetGroups()
-    {
-        throw new NotImplementedException();
-    }
+        => dbContext.Groups.AsNoTracking().ToList();
+    
 
     public Group? GetGroupById(int id)
-    {
-        throw new NotImplementedException();
-    }
+        => dbContext.Groups.AsNoTracking().FirstOrDefault(x => x.Id == id);
 
     public bool CreateGroup(Group group)
     {
-        throw new NotImplementedException();
+        Group? existingGroup = dbContext.Groups.AsNoTracking()
+            .FirstOrDefault(x => x.Name == group.Name);
+        if (existingGroup != null)
+            return false;
+        dbContext.Groups.Add(group);
+        int res = dbContext.SaveChanges();
+        if (res == 0)
+            return false;
+        return true;
     }
 
     public bool UpdateGroup(Group group)
     {
-        throw new NotImplementedException();
+        Group? existingGroup = dbContext.Groups.AsNoTracking()
+            .FirstOrDefault(x => x.Id == group.Id);
+        if (existingGroup == null)
+            return false;
+        
+        dbContext.Groups.Update(group);
+        int res = dbContext.SaveChanges();
+        if (res == 0)
+            return false;
+        return true;
     }
 
     public bool DeleteGroup(int id)
     {
-        throw new NotImplementedException();
+        Group? existingGroup = dbContext.Groups.AsNoTracking()
+            .FirstOrDefault(x => x.Id == id);
+        if (existingGroup == null)
+            return false;
+        
+        dbContext.Groups.Remove(existingGroup);
+        int res = dbContext.SaveChanges();
+        if (res == 0)
+            return false;
+        return true;
     }
 }

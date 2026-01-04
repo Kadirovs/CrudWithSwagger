8f7ee3c3e56276f222a329af40f974d177999eb8
Kadirovs <kadirovkamoliddin24@gmail.com>
2026-01-04 21:59:37 +0400

    git commit

--- src/Domain/Entities/Group.cs
+++ src/Domain/Entities/Group.cs
@@ -4,11 +4,6 @@ public sealed class Group : BaseEntity
 {
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
-    public override string ToString()
-    {
-        return base.ToString();
-    }
-
     public int CourseId { get; set; }
     public Course Course { get; set; } = null!;
 
--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -22,7 +22,7 @@ app.MapGet("/datetimeoffset", () => DateTimeOffset.Now);
  * Everything was burned
  * Think fast
  * Do it first okay to me
- * Finally I solved the what's wrond
+ * Finally I solved the what's the wrong thing
  */
 app.UseSwagger();
 app.UseSwaggerUI(); 

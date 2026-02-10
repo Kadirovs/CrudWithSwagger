98fa0795c48930685ed4ae6ecd93d1c47eb8491d
Kadirovs <kadirovkamoliddin24@gmail.com>
2026-02-10 08:24:08 +0400

    git commit

--- src/Domain/Entities/Course.cs
+++ src/Domain/Entities/Course.cs
@@ -6,4 +6,7 @@ public sealed class Course : BaseEntity
     public string? Description { get; set; }
     public decimal Price { get; set; }
     public List<Group> Groups { get; set; } = [];
+    /*
+     *Hell
+     */
 }
